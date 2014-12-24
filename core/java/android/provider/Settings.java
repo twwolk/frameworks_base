9be3da3c4a000d52070cc9b8829e9f35f099f71a
ezio84-xda <brabus84@gmail.com>
2014-12-24 14:26:46 +0100

    Statusar: bring back Network indicators [1/2]
    source by chameleonos - omnirom - simpleaosp - optipop
    (thanks for sharing your works, guys! )
    
    ezio84: change  attr "min" to "minim" ("min" is
    already declared)
    
    Change-Id: Ic3ec9f2826060a188d5c01dce40a9a3f1b54c49e
    
    Conflicts:
    	packages/SystemUI/res/values/dimens.xml
    
    Conflicts:
    	packages/SystemUI/res/values/dimens.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3222,13 +3222,35 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_MEDIA_STREAM = "volume_keys_control_media_stream";
 
         /**
-          * Volume keys control cursor in text fields (default is 0)
-          * 0 - Disabled
-          * 1 - Volume up/down moves cursor left/right
-          * 2 - Volume up/down moves cursor right/left
-          * @hide
-          */
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
+         * Network traffic indicator, goes from least to greatest significant bitwise
+         * 0 = Display up-stream traffic if set
+         * 1 = Display down-stream traffic if set
+         * 2 = Show as Byte/s if set
+         * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Whether or not to hide the network traffic indicator when there is no activity
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
+
+        /**
+         * Network traffic inactivity threshold (default is 10 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
          * Override and forcefully disable the fullscreen keyboard
