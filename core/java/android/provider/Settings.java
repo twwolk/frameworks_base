5cc5a0065c19f7a82d00afd173ed62a38f81ee2a
maxwen <max.weninger@gmail.com>
2015-01-18 12:54:29 -0500

    [1/2] base: allow unlinking ringer with notification volume
    Forward port from 4.4
    includes also https://github.com/omnirom/android_frameworks_base/commit/c0fae397998268dd34e01c78118852ede6bb31dd
    
    (cherry picked from commit 569a563c9f3594446e510286b41fe712ffc71976)
    
    Change-Id: I635a8cb2f9942462af7c82668f04c711f6fbf84c
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3784,6 +3784,13 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
