f4a92c4b8dbab828d2219358fe657ce080bfde79
DvTonder <david.vantonder@gmail.com>
2015-03-29 14:13:51 -0400

    Base: Allow/Prevent notification light in Zen mode (1 of 2)
    This allows the user to prevent the notification lights from
    showing during Zen mode as per Android 5.0.x
    
    Change-Id: I831c475204c8647d8ee281094aca837ee9594eb4
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3798,6 +3798,12 @@ public final class Settings {
         public static final String VOLUME_PANEL_EXPANDED = "volume_link_expanded";
 
         /**
+         * Whether the notification light will be allowed when in zen mode during downtime
+         * @hide
+         */
+        public static final String ALLOW_LIGHTS = "allow_lights";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -3877,7 +3883,8 @@ public final class Settings {
             PHONE_BLACKLIST_NOTIFY_ENABLED,
             PHONE_BLACKLIST_PRIVATE_NUMBER_MODE,
             PHONE_BLACKLIST_UNKNOWN_NUMBER_MODE,
-            PHONE_BLACKLIST_REGEX_ENABLED
+            PHONE_BLACKLIST_REGEX_ENABLED,
+            ALLOW_LIGHTS
         };
 
         /**
