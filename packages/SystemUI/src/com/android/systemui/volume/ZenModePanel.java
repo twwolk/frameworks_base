omnirom/android_frameworks_base_old

[1/3] base: alarms and no interrupt zen mode
allow to exclude alarms from no interruptions

Change-Id: I0861807999c8827c953d730f9b02b08815d8e4a1

packages/SystemUI/src/com/android/systemui/volume/ZenModePanel.java
@@ -321,11 +321,11 @@ public class ZenModePanel extends LinearLayout {
         mZenSubhead.setVisibility(!mHidden && !zenOff ? VISIBLE : GONE);
         mZenSubheadExpanded.setVisibility(expanded ? VISIBLE : GONE);
         mZenSubheadCollapsed.setVisibility(!expanded ? VISIBLE : GONE);
-        mMoreSettings.setVisibility(zenImportant && expanded ? VISIBLE : GONE);
+        mMoreSettings.setVisibility(expanded ? VISIBLE : GONE);
         mZenConditions.setVisibility(!zenOff && expanded ? VISIBLE : GONE);
 
         if (zenNone) {
-            mZenSubheadExpanded.setText(R.string.zen_no_interruptions_with_warning);
+            mZenSubheadExpanded.setText(R.string.zen_no_interruptions);
             mZenSubheadCollapsed.setText(mExitConditionText);
         } else if (zenImportant) {
             mZenSubheadExpanded.setText(R.string.zen_important_interruptions);

twwolk/frameworks_base

Change-Id: Ibfd1a1ff3565a5da67fbbfcca742bae14d855990
Conflicts:
	packages/SystemUI/res/values/slim_strings.xml

packages/SystemUI/src/com/android/systemui/volume/ZenModePanel.java
@@ -408,11 +408,15 @@ public class ZenModePanel extends LinearLayout {
         mZenSubhead.setVisibility(!mHidden && !zenOff ? VISIBLE : GONE);
         mZenSubheadExpanded.setVisibility(expanded ? VISIBLE : GONE);
         mZenSubheadCollapsed.setVisibility(!expanded ? VISIBLE : GONE);
-        mMoreSettings.setVisibility(zenImportant && expanded ? VISIBLE : GONE);
+        mMoreSettings.setVisibility(expanded ? VISIBLE : GONE);
         mZenConditions.setVisibility(!zenOff && expanded ? VISIBLE : GONE);
 
         if (zenNone) {
-            mZenSubheadExpanded.setText(R.string.zen_no_interruptions_with_warning);
+            if (mController.isAllowAlarms()) {
+                mZenSubheadExpanded.setText(R.string.zen_no_interruptions_but_alarm);
+            } else {
+                mZenSubheadExpanded.setText(R.string.zen_no_interruptions_with_warning);
+            }
             mZenSubheadCollapsed.setText(mExitConditionText);
         } else if (zenImportant) {
             mZenSubheadExpanded.setText(R.string.zen_important_interruptions);